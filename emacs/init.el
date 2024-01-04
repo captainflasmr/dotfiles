6a6f8510f7033c64a63e3004e3fda4b2a98d6f29
james@dyerdwelling.family <james@dyerdwelling.family>
2024-01-04 17:43:46 +0000

    improved selected-window-accent to fringe left right and more robust

--- emacs/init.el
+++ emacs/init.el
@@ -222,7 +222,7 @@
 (define-key my-jump-keymap (kbd "o") (lambda () (interactive) (find-file "~/.config/emacs/emacs--init.org")))
 (define-key my-jump-keymap (kbd "p") 'emms)
 (define-key my-jump-keymap (kbd "r") (lambda () (interactive) (find-file "~/DCIM/Screenshots")))
-(define-key my-jump-keymap (kbd "t") 'customize-themes)
+(define-key my-jump-keymap (kbd "h") 'customize-themes)
 (define-key my-jump-keymap (kbd "w") (lambda () (interactive) (find-file "~/DCIM/content/")))
 (global-set-key (kbd "M-m") my-jump-keymap)
 
@@ -244,7 +244,7 @@
 (define-key my-win-keymap (kbd "o") 'visual-fill-column-mode)
 (define-key my-win-keymap (kbd "p") 'variable-pitch-mode)
 (define-key my-win-keymap (kbd "r") 'org-modern-mode)
-(define-key my-win-keymap (kbd "s") 'spacious-padding-mode)
+(define-key my-win-keymap (kbd "s") 'my/toggle-internal-border-width)
 (define-key my-win-keymap (kbd "t") 'org-tidy-toggle)
 (define-key my-win-keymap (kbd "u") 'toggle-truncate-lines)
 (define-key my-win-keymap (kbd "v") 'visual-line-mode)
@@ -403,6 +403,7 @@
 ;;
 ;; -> keybinding
 ;;
+(global-set-key (kbd "M-e") 'dired-jump)
 (global-set-key (kbd "M-H") 'tab-bar-switch-to-next-tab)
 (global-set-key (kbd "M-L") 'tab-bar-switch-to-prev-tab)
 (global-set-key (kbd "M-=") 'count-words)
@@ -557,13 +558,13 @@
 ;; -> custom-settings
 ;;
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(custom-enabled-themes '(doom-one))
-  '(warning-suppress-log-types '((frameset)))
-  '(warning-suppress-types '((frameset))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(ef-cherie))
+ '(warning-suppress-log-types '((frameset)))
+ '(warning-suppress-types '((frameset))))
 
 ;;
 ;; -> defun
@@ -1827,87 +1828,134 @@
        :right-divider-width 6
        :scroll-bar-width 8)))
 
+(defvar my/internal-border-width 15 "Default internal border width for toggling.")
+
+(defun my/toggle-internal-border-width (&optional value)
+  (interactive "P")
+  (let ((new-value (if value
+                     value
+                     (if (= (or (frame-parameter nil 'internal-border-width) 0)
+                           0)
+                       my/internal-border-width
+                       0))))
+    (modify-all-frames-parameters `((internal-border-width . ,new-value)))))
+
 (use-package lorem-ipsum)
 
-
-(defun selected-window-accent ()
+(defun selected-window-accent (&optional custom-accent-colour)
   (interactive)
-  (set-face-background 'fringe "#77002e")
-  (walk-windows
-    (lambda (window)
-        (if (eq window (selected-window))
-          (progn
-            (set-window-margins window 1 0)
-            (with-selected-window window
-              (if (eq visual-fill-column-mode t)
-              (visual-fill-column-mode t)))
-            (set-window-fringes window 10 0 t nil))
-          (progn
-            (set-window-margins window 2 0)
-            (with-selected-window window
-              (if (eq visual-fill-column-mode t)
-              (visual-fill-column-mode t)))
-            (set-window-fringes window 0 0 t nil))
-          )
-        )
-    nil t))
+  (let* ((init-accent-colour (or custom-accent-colour (face-attribute 'highlight :background)))
+         (accent-offset (if (string-greaterp init-accent-colour "#888888888888") -10 -10))
+         (accent-bg-colour
+           (color-desaturate-name
+             (color-darken-name init-accent-colour accent-offset) 20))
+         (accent-fg-colour (if (string-greaterp accent-bg-colour "#888888888888") "#000000" "#ffffff")))
+    (set-face-attribute 'fringe nil :background accent-bg-colour)
+    (set-face-attribute 'mode-line-active nil :background accent-bg-colour :foreground accent-fg-colour)
+    (walk-windows
+     (lambda (window)
+       (if (eq window (selected-window))
+           (progn
+             (set-window-margins window 1 0)
+             (with-selected-window window
+               (if (eq visual-fill-column-mode t)
+                   (visual-fill-column-mode t)))
+             (set-window-fringes window 10 10 t nil))
+         (progn
+           (set-window-margins window 2 0)
+           (with-selected-window window
+             (if (eq visual-fill-column-mode t)
+                 (visual-fill-column-mode t)))
+           (set-window-fringes window 0 0 t nil))
+         )
+       )
+      nil t)))
 
 (add-hook 'window-configuration-change-hook 'selected-window-accent)
+;; (add-hook 'window-configuration-change-hook (lambda () (interactive)(selected-window-accent "blue")))
 (add-hook 'window-state-change-hook 'selected-window-accent)
+;; (add-hook 'window-state-change-hook (lambda () (interactive)(selected-window-accent "blue")))
 
-(defun subtract-weight (weight-str avg-loss)
-  "Subtract AVG-LOSS pounds from WEIGHT-STR given in 'stones:pounds' format."
-  (let* ((stones-pounds (split-string weight-str ":"))
-         (stones (string-to-number (car stones-pounds)))
-         (pounds (string-to-number (cadr stones-pounds)))
-         (total-pounds (+ pounds (* stones 14)))      ;; Convert stones to pounds
-         (new-total-pounds (- total-pounds avg-loss)) ;; Subtract weight loss
-         (new-stones (truncate (/ new-total-pounds 14))) ;; Calculate new stones
-         (new-pounds (mod new-total-pounds 14)))      ;; Calculate remaining pounds
-    (format "%d:%d" new-stones new-pounds)))         ;; Format new weight
+  (defun subtract-weight (weight-str avg-loss)
+    "Subtract AVG-LOSS pounds from WEIGHT-STR given in 'stones:pounds' format."
+    (let* ((stones-pounds (split-string weight-str ":"))
+            (stones (string-to-number (car stones-pounds)))
+            (pounds (string-to-number (cadr stones-pounds)))
+            (total-pounds (+ pounds (* stones 14)))      ;; Convert stones to pounds
+            (new-total-pounds (- total-pounds avg-loss)) ;; Subtract weight loss
+            (new-stones (truncate (/ new-total-pounds 14))) ;; Calculate new stones
+            (new-pounds (mod new-total-pounds 14)))      ;; Calculate remaining pounds
+      (format "%d:%d" new-stones new-pounds)))         ;; Format new weight
 
-(defun extrapolate-weight-loss (num-weeks)
-  "Extrapolate weight loss for NUM-WEEKS using the last 'av/pd' value in the org-table."
-  (interactive "p")
-  (save-excursion
-    (let ((last-avg-loss 2.9)
-           (last-date "")
-           (week 0)
-           (next-date ""))
-      (print num-weeks)
-      (when (org-table-p)
-        (goto-char (org-table-end))
-        ;; Find the last date and week number
-        (search-backward-regexp "|\\s-?\\([0-9]+\\)\\s-?|\\s-?<\\([0-9-]+\\)" nil t)
-        (setq week (string-to-number (match-string 1)))
-        (setq last-date (match-string 2))
-        (setq last-weight "16:10")
+  (defun extrapolate-weight-loss (num-weeks)
+    "Extrapolate weight loss for NUM-WEEKS using the last 'av/pd' value in the org-table."
+    (interactive "p")
+    (save-excursion
+      (let ((last-avg-loss 2.9)
+             (last-date "")
+             (week 0)
+             (next-date ""))
+        (print num-weeks)
+        (when (org-table-p)
+          (goto-char (org-table-end))
+          ;; Find the last date and week number
+          (search-backward-regexp "|\\s-?\\([0-9]+\\)\\s-?|\\s-?<\\([0-9-]+\\)" nil t)
+          (setq week (string-to-number (match-string 1)))
+          (setq last-date (match-string 2))
+          (setq last-weight "16:10")
 
-        (goto-char (org-table-end))
+          (goto-char (org-table-end))
 
-        ;; Loop for num-weeks to generate new lines
-        (dotimes (i num-weeks)
-          (setq next-date
-            (format-time-string "<%Y-%m-%d %a>"
-              (time-add (org-time-string-to-time last-date)
-                (days-to-time (+ (* i 7) 7))))) ;; add 7 days per week
-          (setq week (+ week 1))
-          (insert (format "| %d | %s | %s | | | | | | |\n"
-                    week next-date (subtract-weight last-weight (* (+ i 1) last-avg-loss)))))
+          ;; Loop for num-weeks to generate new lines
+          (dotimes (i num-weeks)
+            (setq next-date
+              (format-time-string "<%Y-%m-%d %a>"
+                (time-add (org-time-string-to-time last-date)
+                  (days-to-time (+ (* i 7) 7))))) ;; add 7 days per week
+            (setq week (+ week 1))
+            (insert (format "| %d | %s | %s | | | | | | |\n"
+                      week next-date (subtract-weight last-weight (* (+ i 1) last-avg-loss)))))
+          )
         )
       )
-    )
-  (org-table-align))
+    (org-table-align))
 
-;; tab-bar experimentation
+  ;; tab-bar experimentation
 
-(add-to-list 'tab-bar-format #'tab-bar-format-menu-bar)
+  (add-to-list 'tab-bar-format #'tab-bar-format-menu-bar)
 
-(define-icon tab-bar-menu-bar nil
-  '((emoji "🫥")
-     (text "Menu" :face tab-bar-tab-inactive))
-  "Icon for the menu bar."
-  :version "29.1"
-  :help-echo "Menu bar")
+  (define-icon tab-bar-menu-bar nil
+    '((emoji "🫥")
+       (text "Menu" :face tab-bar-tab-inactive))
+    "Icon for the menu bar."
+    :version "29.1"
+    :help-echo "Menu bar")
 
-(setq tab-bar-menu-bar-button (icon-string 'tab-bar-menu-bar))
+  (setq tab-bar-menu-bar-button (icon-string 'tab-bar-menu-bar))
+
+  (use-package ox-epub)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t :background "#282828")))
+ '(header-line ((t :box (:line-width 4 :color "#282828" :style nil))))
+ '(header-line-highlight ((t :box (:color "#ebdbb2"))))
+ '(line-number ((t :background "#282828")))
+ '(mode-line ((t :box (:line-width 6 :color "#665c54" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#a7a7a7" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#ebdbb2"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#151515" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#504945" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#282828" :style nil))))
+ '(whitespace-missing-newline-at-eof ((t (:foreground "#666566656665"))))
+ '(whitespace-newline ((t (:foreground "#666566656665"))))
+ '(whitespace-space ((t (:foreground "#666566656665"))))
+ '(whitespace-space-after-tab ((t (:foreground "#666566656665"))))
+ '(whitespace-space-before-tab ((t (:foreground "#666566656665"))))
+ '(whitespace-tab ((t (:foreground "#666566656665"))))
+ '(whitespace-trailing ((t (:foreground "#666566656665"))))
+ '(window-divider ((t :background "#282828" :foreground "#282828")))
+ '(window-divider-first-pixel ((t :background "#282828" :foreground "#282828")))
+ '(window-divider-last-pixel ((t :background "#282828" :foreground "#282828"))))
