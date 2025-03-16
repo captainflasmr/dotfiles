303abafd660984296c0168ba41d7854d2971a4a0
james dyer <captainflasmr@gmail.com>
2025-03-16 09:00:26 +0000

    fish config tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,16 @@ if status is-interactive
     set -g fish_greeting ""
 end
 
+# Add ~/bin to PATH if it's not already included
+if not contains "~/bin" $PATH
+    set -x PATH ~/bin $PATH
+end
+
 alias l='exa -al --color=always --group-directories-first --icons' # preferred listing
 
 export WVKBD_LANDSCAPE_HEIGHT=400
+export SSH_ASKPASS=/usr/bin/qt4-ssh-askpass
+export SSH_ASKPASS_REQUIRE=prefer
+# eval `keychain --eval --noask ~/.ssh/id_rsa`
 
 alias upall='upall.sh'
