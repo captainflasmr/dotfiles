3107e7bd17795ab255af43213638d507cbcb4381
james dyer <captainflasmr@gmail.com>
2026-06-15 06:27:38 +0100

    tweaks

--- fish/config.fish
+++ fish/config.fish
@@ -11,10 +11,8 @@ end
 alias l='exa -al --color=always --group-directories-first --icons' # preferred listing
 
 export WVKBD_LANDSCAPE_HEIGHT=400
-export SSH_ASKPASS=/usr/bin/qt4-ssh-askpass
-export SSH_ASKPASS_REQUIRE=prefer
 export OLLAMA_API_BASE_URL=172.17.0.1
-# eval `keychain --eval --noask ~/.ssh/id_rsa`
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
 
 # export CLOUDFLARE_API_TOKEN="zLyUWJ21jPl0UJJo5THbwd7hIErJY4HuO6IS_Sne"
 # export CLOUDFLARE_API_TOKEN="d596c25533e9063de8ef5b28fc43a1057caab"
