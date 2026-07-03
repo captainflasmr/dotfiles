efc8799a321d87739022ebeb3c64e376000d6bfc
james dyer <captainflasmr@gmail.com>
2026-07-03 07:15:12 +0100

    tidying up sway

--- sway/scripts/screenshot_display.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-output_id=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused).name')
-grim -o $output_id - | swappy -f -
--- sway/scripts/wkill.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-FOCUSED_APP_ID=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid')
-kill -9 $FOCUSED_APP_ID
