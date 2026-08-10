63c53937f9980c950cee7152920e32a115ab63a8
james dyer <captainflasmr@gmail.com>
2026-08-10 08:32:44 +0100

    a few tweaks

--- sway/scripts/auto_output.sh
+++ sway/scripts/auto_output.sh
@@ -5,7 +5,7 @@
 swaymsg -t get_outputs | jq -r '
     .[] | select(.active == true) |
     (if .current_mode.width >= 1920 then "1.0"
-     else "0.8" end) as $scale |
+     else "1.0" end) as $scale |
     "swaymsg output \(.name) scale \($scale)"
 ' | while read -r cmd; do
     eval "$cmd"
